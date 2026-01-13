2c6b0794b0277cf3c5caa4d88a0fcc9ab878c82b
Ayush Pandey <apayush.20@gmail.com>
2026-01-13 11:00:43 +0530

    Backend URL added

--- gigflow-frontend/src/api/axios.js
+++ gigflow-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5050/api",
+  baseURL: "https://gigflow-4ohh.onrender.com",
   withCredentials: true, // IMPORTANT for cookies
 });
 
